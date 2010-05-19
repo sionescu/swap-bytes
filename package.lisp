5b3866d3d5ffd4ac4cf7d42429100d390f23d5ee
Stelian Ionescu <sionescu@cddr.org>
2010-05-20 00:31:25 +0800

    Add HTONS, NTOHS, HTONL, NTOHL, HTONQ and NTOHQ

--- package.lisp
+++ package.lisp
@@ -1,4 +1,5 @@
 (defpackage :swap-bytes
   (:use #:cl .
         #+(and sbcl (or x86 x86-64)) (#:sb-c #:sb-assem))
-  (:export #:swap-bytes-16 #:swap-bytes-32 #:swap-bytes-64))
+  (:export #:swap-bytes-16 #:swap-bytes-32 #:swap-bytes-64
+           #:htons #:ntohs #:htonl #:ntohl #:htonq #:ntohq))
--- portable.lisp
+++ portable.lisp
@@ -35,3 +35,58 @@
      (shift #x00FF0000000000 -24)
      (shift #xFF000000000000 -40)
      (ash integer -56))))
+
+
+(declaim (inline htons))
+(defun htons (integer)
+  "Convert (unsigned-byte 16) from host order(little- or big-endian)
+to network order(always big-endian)."
+  (declare (type (unsigned-byte 16) integer)
+           (optimize (speed 3) (safety 0) (debug 0)))
+  #+little-endian (swap-bytes-16 integer)
+  #+big-endian    integer)
+
+(declaim (inline ntohs))
+(defun ntohs (integer)
+  "Convert (unsigned-byte 16) from network order(always big-endian) to
+host order(little- or big-endian)."
+  (declare (type (unsigned-byte 16) integer)
+           (optimize (speed 3) (safety 0) (debug 0)))
+  #+little-endian (swap-bytes-16 integer)
+  #+big-endian    integer)
+
+(declaim (inline htonl))
+(defun htonl (integer)
+  "Convert (unsigned-byte 32) from host order(little- or big-endian)
+to network order(always big-endian)."
+  (declare (type (unsigned-byte 32) integer)
+           (optimize (speed 3) (safety 0) (debug 0)))
+  #+little-endian (swap-bytes-32 integer)
+  #+big-endian    integer)
+
+(declaim (inline ntohl))
+(defun ntohl (integer)
+  "Convert (unsigned-byte 32) from network order(always big-endian) to
+host order(little- or big-endian)."
+  (declare (type (unsigned-byte 32) integer)
+           (optimize (speed 3) (safety 0) (debug 0)))
+  #+little-endian (swap-bytes-32 integer)
+  #+big-endian    integer)
+
+(declaim (inline htonq))
+(defun htonq (integer)
+  "Convert (unsigned-byte 64) from host order(little- or big-endian)
+to network order(always big-endian)."
+  (declare (type (unsigned-byte 64) integer)
+           (optimize (speed 3) (safety 0) (debug 0)))
+  #+little-endian (swap-bytes-64 integer)
+  #+big-endian    integer)
+
+(declaim (inline ntohq))
+(defun ntohq (integer)
+  "Convert (unsigned-byte 64) from network order(always big-endian) to
+host order(little- or big-endian)."
+  (declare (type (unsigned-byte 64) integer)
+           (optimize (speed 3) (safety 0) (debug 0)))
+  #+little-endian (swap-bytes-64 integer)
+  #+big-endian    integer)
