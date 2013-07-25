70178c36ab44f75aed7de0d3db86b7acc44afad4
Stelian Ionescu <sionescu@cddr.org>
2013-07-25 12:49:59 +0200

    Pass :OVERWRITE-FNDB-SILENTLY T to DEFKNOWN and remove ASDF kludge

--- sbcl-defknowns.lisp
+++ sbcl-defknowns.lisp
@@ -7,12 +7,15 @@
 
 #+x86
 (defknown swap-bytes-16 ((unsigned-byte 16)) (unsigned-byte 16)
-    (movable foldable flushable))
+    (movable foldable flushable)
+  :overwrite-fndb-silently t)
 
 #+(or x86 x86-64)
 (defknown swap-bytes-32 ((unsigned-byte 32)) (unsigned-byte 32)
-    (movable foldable flushable))
+    (movable foldable flushable)
+  :overwrite-fndb-silently t)
 
 #+x86-64
 (defknown swap-bytes-64 ((unsigned-byte 64)) (unsigned-byte 64)
-    (movable foldable flushable))
+    (movable foldable flushable)
+  :overwrite-fndb-silently t)
