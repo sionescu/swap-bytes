8de1a6e0ebdc02b0fc83b340b27b1b1d89b0ab4e
Stelian Ionescu <sionescu@cddr.org>
2013-07-25 16:09:08 +0200

    SBCL: fix inlining

--- sbcl.lisp
+++ sbcl.lisp
@@ -7,7 +7,7 @@
 #+x86
 (defun swap-bytes-16 (integer)
   (declare (type (unsigned-byte 16) integer))
-  (swap-bytes-16 integer))
+  (%swap-bytes-16 integer))
 
 #+x86-64
 (defun swap-bytes-16 (integer)
@@ -19,7 +19,7 @@
 #+(or x86 x86-64)
 (defun swap-bytes-32 (integer)
   (declare (type (unsigned-byte 32) integer))
-  (swap-bytes-32 integer))
+  (%swap-bytes-32 integer))
 
 #+x86
 (defun swap-bytes-64 (integer)
@@ -40,4 +40,4 @@
 #+x86-64
 (defun swap-bytes-64 (integer)
   (declare (type (unsigned-byte 64) integer))
-  (swap-bytes-64 integer))
+  (%swap-bytes-64 integer))
