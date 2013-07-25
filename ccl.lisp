255abfe82242f72697ca03946d5bc09cc3178c81
Stelian Ionescu <sionescu@cddr.org>
2013-07-25 13:38:09 +0200

    CCL: define LAP functions in package CCL

--- ccl.lisp
+++ ccl.lisp
@@ -1,16 +1,16 @@
 ;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
 
-(in-package :swap-bytes)
+(in-package :ccl)
 
-(ccl::defx8632lapfunction %swap-bytes-16 ((unsigned-byte arg_z))
+(defx8632lapfunction swap-bytes::%swap-bytes-16 ((unsigned-byte arg_z))
   (xchg  (% arg_z.bh)
          (% arg_z.b))
   (single-value-return))
 
-(ccl::defx8632lapfunction %swap-bytes-32 ((integer arg_z))
+(defx8632lapfunction swap-bytes::%swap-bytes-32 ((integer arg_z))
   (bswapl (% arg_z))
   (single-value-return))
 
-(ccl::defx86lapfunction %swap-bytes-64 ((integer arg_z))
+(defx86lapfunction swap-bytes::%swap-bytes-64 ((integer arg_z))
   (bswapq (% arg_z))
   (single-value-return))
