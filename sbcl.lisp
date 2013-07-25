3cd5677faef672acfcc3bd4caf9c6b38c5eca354
Stelian Ionescu <sionescu@cddr.org>
2013-07-25 13:43:24 +0200

    Add reader conditional for clarity

--- sbcl.lisp
+++ sbcl.lisp
@@ -7,6 +7,7 @@
   (declare (type (unsigned-byte 16) integer))
   (swap-bytes-16 integer))
 
+#+(or x86 x86-64)
 (defun swap-bytes-32 (integer)
   (declare (type (unsigned-byte 32) integer))
   (swap-bytes-32 integer))
