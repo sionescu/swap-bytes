88c254e4b3eb14588fd19cb433340e506d333ee7
Stelian Ionescu <sionescu@cddr.org>
2013-07-25 13:23:04 +0200

    More precise reader conditionals on SBCL

--- sbcl.lisp
+++ sbcl.lisp
@@ -5,17 +5,16 @@
 
 (in-package :swap-bytes)
 
-#+(or x86 x86-64)
+#+x86
 (defun swap-bytes-16 (integer)
   (declare (type (unsigned-byte 16) integer))
   (swap-bytes-16 integer))
 
-#+(or x86 x86-64)
 (defun swap-bytes-32 (integer)
   (declare (type (unsigned-byte 32) integer))
   (swap-bytes-32 integer))
 
-#+(or x86-64)
+#+x86-64
 (defun swap-bytes-64 (integer)
   (declare (type (unsigned-byte 64) integer))
   (swap-bytes-64 integer))
